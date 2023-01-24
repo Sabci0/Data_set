79e0e91d1ea0b0d9f1756809985fc95fa6cec5e6
Bartlomiej Sabiniok <barteksabcio@gmail.com>
2023-01-24 17:55:18 +0100

    init 2 files for first and second day of helenge and add index.html file

---
+++ data_sets/day_2_set.js
@@ -0,0 +1,44 @@
+const trucks = [
+    {
+        truck: "Scania",
+        hp: "560",
+        truck_driver: "Janusz",
+        weight: 40000,
+        favourite_meal: ["Pizza", "Burger", "Fries"]
+    },
+    {
+        truck: "Renault",
+        hp: "420",
+        truck_driver: "Jarek",
+        weight: 35000,
+        favourite_meal: ["Kebab", "Burger", "Fries"]
+    },
+    {
+        truck: "Volvo",
+        hp: "700",
+        truck_driver: "Zenek",
+        weight: 37000,
+        favourite_meal: ["Kebab", "Burger", "Chciken"]
+    },
+    {
+        truck: "Mercedes",
+        hp: "460",
+        truck_driver: "Jurek",
+        weight: 16000,
+        favourite_meal: ["Bigos", "Burger", "Fries"]
+    },
+    {
+        truck: "Man",
+        hp: "440",
+        truck_driver: "Donald",
+        weight: 20000,
+        favourite_meal: ["Pizza", "Steak", "Ice cream"]
+    },
+    {
+        truck: "Iveco",
+        hp: "380",
+        truck_driver: "Antoni",
+        weight: 25000,
+        favourite_meal: ["Pizza", "Steak", "Spaghetti"]
+    },
+]
