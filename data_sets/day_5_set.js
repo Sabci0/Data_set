f17ab34219bae3eef9da42195c9e8ac881bef832
Bartlomiej Sabiniok <barteksabcio@gmail.com>
2023-01-28 19:29:26 +0100

    init day 5 file

---
+++ data_sets/day_5_set.js
@@ -0,0 +1,72 @@
+const football = [
+    {
+        team: "Real Madrid",
+        foundingYear: 1902,
+        nationality: "Spain",
+        coach: {
+            name: "Carlo Ancelotti",
+            age: 63,
+            nationality: "Italy"
+        },
+        titles: {
+            ChampionsLeague: 13,
+            LeagueTitles: 34,
+        }
+    },
+    {
+        team: "Barcelona",
+        foundingYear: 1899,
+        nationality: "Spain",
+        coach: {
+            name: "Xavier Hernández",
+            age: 42,
+            nationality: "Spain"
+        },
+        titles: {
+            ChampionsLeague: 5,
+            LeagueTitles: 26,
+        }
+    },
+    {
+        team: "Bayern Munich",
+        foundingYear: 1900,
+        nationality: "Germany",
+        coach: {
+            name: "Hansi Flick",
+            age: 55,
+            nationality: "Germany"
+        },
+        titles: {
+            ChampionsLeague: 6,
+            LeagueTitles: 31,
+        }
+    },
+    {
+        team: "Manchester United",
+        foundingYear: 1878,
+        nationality: "England",
+        coach: {
+            name: "Ole Gunnar Solskjaer",
+            age: 46,
+            nationality: "Norway"
+        },
+        titles: {
+            ChampionsLeague: 3,
+            LeagueTitles: 20,
+        }
+    },
+    {
+        team: "Paris Saint-Germain",
+        foundingYear: 1970,
+        nationality: "France",
+        coach: {
+            name: "Mauricio Pochettino",
+            age: 49,
+            nationality: "Argentina"
+        },
+        titles: {
+            ChampionsLeague: 0,
+            LeagueTitles: 8,
+        }
+    }
+];
