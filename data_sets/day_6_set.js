a1f6d0557508acce5de2858cda9c3b5f19a7b8ce
Sabci0 <barteksabcio@gmail.com>
2023-01-30 01:12:51 +0100

    make final 3 functions in day 6

--- data_sets/day_6_set.js
+++ data_sets/day_6_set.js
@@ -45,7 +45,36 @@ const jazz = [
     }
 
 ]
+function characterConversion(elements){
+
+        for (const element of elements){
+            element.listeners = Number(element.listeners.replace(/\./g, ''))
+        }
+        return elements
+}
+
 
 //1. Wyświetl imiona wykonawców, które się urodzili w Stanach Zjednoczonych i są kompozytorami, posortuj ich w zależności od ilości słuchaczy.
+
+function getNames(elements){
+    characterConversion(elements);
+    return elements.filter(({city, musical_instrument}) => city === 'Stany Zjednoczone' && musical_instrument.includes('kompozytor') ).sort((a,b) => b.listeners - a.listeners).map(({name}) => name);
+}
+
+
 //2. Wyświetl imię i najpopularniejszy utwór, który ma najwięcej słuchaczy ze wszystkich.
+
+function getBestHIT(elements){
+    characterConversion(elements);
+    elements.sort((a,b) => b.listeners - a.listeners)
+    return elements[0].name + ' - ' + elements[0].most_popular_song
+}
+
 //3. Wyświetl imiona polskich wykonawców, posortuj według popularności i większej nazwy popularnego utworu.
+
+function getPolishPerformers(elements){
+    characterConversion(elements);
+    return elements.filter(({city})=> city !=='Stany Zjednoczone').sort((a,b) => b.listeners - a.listeners).map(({name,most_popular_song})=> name + ' - ' + most_popular_song)
+}
+
+
